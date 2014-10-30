fd75d1ff1c0db028458eebb7272ebb000ed8284e
Fabio Barone <holistic.computing@gmail.com>
2014-10-29 22:58:59 -0500

    carousel and modal dialog

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
 var app = angular.module('bibliolabs-viz', [
        // 'ui.mask',
-        'ngAnimate',
+       'ngAnimate',
        // 'adminControllers',
        // 'adminServices',
-        'ui.bootstrap',
-        'ngRoute']);
+       'ui.bootstrap',
+       'ngRoute']);
 
 //var adminServices = angular.module('adminServices', ['ngResource']);
 
@@ -23,7 +23,7 @@ app.factory('ItemProvider', function() {
   return {
     get_data: function() {
       //create json here
-      json = {"iniciativas":[{"nombre":"Viajeros del Agua","portada":"http://3.bp.blogspot.com/-QSzBelKhV78/U_td1brks1I/AAAAAAAAA-w/uf6avHYstv0/s1600/Iniciativa.jpg","descripcion":"Saberes y creaciones entorno a la quebrada La Quintana y otros cuerpos de água","descripcion_larga":""},{"nombre":"Huertas Y Járdines","portada":"http://2.bp.blogspot.com/-J2Pn4dPqiOU/U_NK7b8Dy0I/AAAAAAAAA9I/u82MV60RqEo/s1600/Optical_illusion_disc_with_man_pumping_water.gif","descripcion":"Prácticas tradicionales para la siembra de huertas y jardines urbanos en el territorio","descripcion_larga":""}]};
+      json = {"iniciativas":[{"nombre":"Viajeros del Agua","diapositiva":"http://4.bp.blogspot.com/-aFmDEU7P2c4/UBK_CVb06kI/AAAAAAAAABA/j2pG-AuaTp8/s640/598852_175085729288447_716591627_n.jpg","portada":"http://3.bp.blogspot.com/-QSzBelKhV78/U_td1brks1I/AAAAAAAAA-w/uf6avHYstv0/s1600/Iniciativa.jpg","descripcion":"Saberes y creaciones entorno a la quebrada La Quintana y otros cuerpos de água","descripcion_larga":""},{"nombre":"Huertas Y Járdines","diapositiva":"http://2.bp.blogspot.com/-UN1kvNsJUs8/U_tSj68H4gI/AAAAAAAAA-g/udYYWF3VtP4/s1600/Jardines%2By%2BHuertas.png","portada":"http://2.bp.blogspot.com/-J2Pn4dPqiOU/U_NK7b8Dy0I/AAAAAAAAA9I/u82MV60RqEo/s1600/Optical_illusion_disc_with_man_pumping_water.gif","descripcion":"Prácticas tradicionales para la siembra de huertas y jardines urbanos en el territorio","descripcion_larga":""}]};
       return json;
     }
   };
@@ -47,11 +47,53 @@ app.config(['$routeProvider',
 }]);
 
 
-app.controller("VitrinaCtrl", function VitrinaCtrl($scope, $http, ItemService, ItemProvider) {
+app.controller("VitrinaCtrl", function VitrinaCtrl($scope, $http, ItemService, ItemProvider, $modal) {
   $scope.root = {};
   json = ItemService(ItemProvider);
   $scope.root.iniciativas = json.iniciativas;
-  $scope.slides = json.iniciativas;
+
+  $scope.show_detail = function(iniciativa) {
+    $scope.root.selected_iniciativa = iniciativa;
+    $scope.open('lg'); 
+  }
+
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'partials/iniciativa',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.root.selected_iniciativa;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      //$log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+});
+
+
+app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+  $scope.iniciativa = items;
+  $scope.selected = {
+  //  item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
 });
 
 app.controller("MenuCtrl", function MenuCtrl($scope, $http) {
@@ -70,3 +112,7 @@ app.controller("MenuCtrl", function MenuCtrl($scope, $http) {
   }
 });
 
+
+app.controller('CarouselIniciativasCtrl', function ($scope) {
+  $scope.myInterval = 5000;
+});
